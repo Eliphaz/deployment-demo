95cfae9ece077adf968dd4572acce7fbd68c903c
Eliphaz <eliahlander3@gmail.com>
2022-03-16 12:16:41 -0600

    added frontend

--- server/server.js
+++ server/server.js
@@ -7,7 +7,15 @@ app.use(cors())
 app.use(express.json())
 
 app.get('/', (req,res)=>{
-    res.sendFile(path.join(__dirname, '../index.html'))
+    res.sendFile(path.join(__dirname, '../public/index.html'))
+})
+
+app.get('/styles', (req,res)=>{
+    res.sendFile(path.join(__dirname,'../public/index.css'))
+})
+
+app.get('/js', (req,res)=>{
+    res.sendFile(path.join(__dirname,'../public/index.js'))
 })
 
 const port = process.env.PORT || 4005
