7945f536350aeb4d07a8d766200b9f1ee05f2976
Eliphaz <eliahlander3@gmail.com>
2022-03-16 12:29:37 -0600

    added middleware

--- server/server.js
+++ server/server.js
@@ -6,17 +6,19 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
-app.get('/', (req,res)=>{
-    res.sendFile(path.join(__dirname, '../public/index.html'))
-})
+app.use(express.static(path.join(__dirname, '../public')))
 
-app.get('/styles', (req,res)=>{
-    res.sendFile(path.join(__dirname,'../public/index.css'))
-})
+// app.get('/', (req,res)=>{
+//     res.sendFile(path.join(__dirname, '../public/index.html'))
+// })
 
-app.get('/js', (req,res)=>{
-    res.sendFile(path.join(__dirname,'../public/index.js'))
-})
+// app.get('/styles', (req,res)=>{
+//     res.sendFile(path.join(__dirname,'../public/index.css'))
+// })
+
+// app.get('/js', (req,res)=>{
+//     res.sendFile(path.join(__dirname,'../public/index.js'))
+// })
 
 const port = process.env.PORT || 4005
 
